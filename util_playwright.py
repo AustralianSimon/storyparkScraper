a1437092f8220c0230128f3eb27509bd61225548
AustralianSimon <simon@simonwise.au>
2026-06-16 19:08:45 +1000

    Updates to handling headless mode in docker. Removed running docker container as root. Removed port exposure. Time to test.

--- util_playwright.py
+++ util_playwright.py
@@ -4,8 +4,17 @@ from playwright.sync_api import sync_playwright
 def create_browser_instance(visible):
     playwright = sync_playwright().start()
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36"
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context(user_agent=user_agent)
+    browser = playwright.chromium.launch(
+        headless=True,
+        args=[
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+        ]
+    )
+    context = browser.new_context(
+        user_agent=user_agent,
+        viewport={'width': 1280, 'height': 900},
+    )
     return context
 
 def close_instance(browser):
