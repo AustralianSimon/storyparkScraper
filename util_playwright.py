c9b15fb966564f09730e8c309e656ccbe2e6663a
AustralianSimon <simon@simonwise.au>
2026-06-16 18:53:39 +1000

    Back to headless

--- util_playwright.py
+++ util_playwright.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 def create_browser_instance(visible):
     playwright = sync_playwright().start()
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36"
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context(user_agent=user_agent)
     return context
 
